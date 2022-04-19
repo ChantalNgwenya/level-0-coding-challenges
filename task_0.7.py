bb411d52f0f9f578ebadbe666fb471b84dbbd89c
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-20 01:25:09 +0200

    updated file
    Rename Task_0.7.py to task_0.7.py.
    Made the function more clear and easy to understand

--- task_0.7.py
+++ task_0.7.py
@@ -1,24 +1,22 @@
-# function to coverts degrees celsius to degrees fahrenheit
 
-def convert_cel_to_fah(C):
+
+def celsius_to_fahrenheit(C):
     F = C * (9/5) + 32
     return F
 
-# function to convert degrees fahrenheit to degrees celsius
-
-def convert_fah_to_cel(F):
+def fahrenheut_to_celsius(F):
     C = F - 32 * (5/9)
     return C
-# converts celsius to fahrenheit
+
 C = float(input("Enter temperature in degrees Celsius: "))
 
-F = convert_cel_to_fah(C)
+F = celsius_to_fahrenheit(C)
 
 print(f"{C} degrees Celsius = {F} degrees Fahrenheit")
 
-# convets fahrenheit to celsius
+
 F = float( input("Enter temperature in degrees Fahrenheit: "))
 
-C = convert_fah_to_cel(F)
+C = fahrenheit_to_celsius(F)
 
 print(f"{F} degrees fahrenheit = {C} degrees Celsius")
