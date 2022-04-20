8215e20ef0c2fe6b0d46da4904d9b7ce82cae932
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-20 02:25:03 +0200

    updated file

--- task_0.6.py
+++ task_0.6.py
@@ -1,5 +1,4 @@
-# Python program to find the largest
-# number among the three numbers
+
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 c = int(input("Enter third number:"))
