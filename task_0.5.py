a7e5b71ad3bacd4317fd2aad8b6aa71b6ed4a876
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-20 16:46:41 +0200

    updated file

--- task_0.5.py
+++ task_0.5.py
@@ -1,14 +1,14 @@
 
-x = float(input("Enter first side:")
-y =float(input("Enter second side:"))
-z = float(input("Enter third side"))
+x = 5
+y =6
+z = 7
 
 def area_of_triangle(x,y,z,):
     if( x < 0  or y < 0 or z < 0 or x+z <=y or y+z <=x ):
         print ("Not a triangle")
         return
-    s = (x + y + z) /2  # calculates the semi-perimeter
-    # It calculates the area
+    s = (x + y + z) /2  
+    
     area = (s* ( s-x)* (s - y)* (s -z))** 0.5
     print("Area of a triangle is %f" %area)
 
