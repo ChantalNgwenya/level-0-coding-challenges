4004656555cdcf30e5564379ff1ebcbe477f9ae2
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-20 17:07:22 +0200

    updated file

--- task_0.8.py
+++ task_0.8.py
@@ -3,8 +3,7 @@ def convert(sec):
     
 
     hour = sec // 60
-    sec %= 60
-   
+    sec %= 60 
     minutes = sec 
     if (hour==1 and minutes==1):
         print(hour, "hour", minutes, "minute")
