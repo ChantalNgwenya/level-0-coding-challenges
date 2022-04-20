b95c840c886eb4c1e62e6840fde5e5704b435e67
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-20 17:00:10 +0200

    updated file

--- task_0.5.py
+++ task_0.5.py
@@ -1,15 +1,15 @@
 
-x = 5
-y =6
-z = 7
+num1 = 5
+num2 = 6
+num3 = 7
 
-def area_of_triangle(x,y,z,):
-    if( x < 0  or y < 0 or z < 0 or x+z <=y or y+z <=x ):
+def area_of_triangle(num1,num2,num3):
+    if( num1 < 0  or num2 < 0 or num3 < 0 or num1+num3 <=num2 or num2+num3 <=num1 ):
         print ("Not a triangle")
         return
-    s = (x + y + z) /2  
+    sum = (num1 + num2 + num3) /2  
     
-    area = (s* ( s-x)* (s - y)* (s -z))** 0.5
+    area = (sum* ( sum-num1)* (sum - num2)* (sum -num3))** 0.5
     print("Area of a triangle is %f" %area)
 
-area_of_triangle(x,y,z)
+area_of_triangle(num1,num2,num3)
