a56666b4f3d274c094fb1011db99b8ae0768f879
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-22 04:06:07 +0200

    Update task_0.9.py

--- task_0.9.py
+++ task_0.9.py
@@ -1,7 +1,7 @@
 
 def vowels(string):
     
-    for character in string.lower():
+    for character in string:
         if character in "aeiou":
             print(character, end=', ')
     return character
