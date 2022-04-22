f1d81ad828720198145a943ae95a25b6a5aef2ea
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-04-22 04:01:25 +0200

    Update task_0.7.py

--- task_0.7.py
+++ task_0.7.py
@@ -4,7 +4,7 @@ def celsius_to_fahrenheit(Celsius):
     Fahrenheit = Celsius * (9/5) + 32
     return Fahrenheit
 
-def fahrenheut_to_celsius(Fahrenheit):
+def fahrenheit_to_celsius(Fahrenheit):
     Celsius = Fahrenheit - 32 * (5/9)
     return Celsius
 
