9e370a5ce6dc885d6a100b3dade393a187eff4cc
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-06-13 16:06:29 +0200

    Update task_0.5.py

--- task_0.5.py
+++ task_0.5.py
@@ -4,7 +4,7 @@ def area_of_triangle(num1,num2,num3):
         return None
     semi_perimeter = (num1 + num2 + num3) /2  
     
-    area = (semi_perieter* ( semi_perimeter-num1)* (semi_perimeter - num2)* (semi_primeter -num3))** 0.5
+    area = (semi_perimeter* ( semi_perimeter-num1)* (semi_perimeter - num2)* (semi_primeter -num3))** 0.5
     print("Area of a triangle is %f" %area)
 
 area_of_triangle(5,6,7)
