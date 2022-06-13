1e3e86c229bb4bec522bfcd2e0af9fc691deadd1
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-06-13 16:16:38 +0200

    Update task_0.7.py

--- task_0.7.py
+++ task_0.7.py
@@ -2,21 +2,14 @@
 
 def celsius_to_fahrenheit(Celsius):
     Fahrenheit = Celsius * (9/5) + 32
-    return Fahrenheit
+    return print(f"{Celsius} degrees Celsius = {Fahrenheit} degrees Fahrenheit")
+
 
 def fahrenheit_to_celsius(Fahrenheit):
     Celsius = Fahrenheit - 32 * (5/9)
-    return Celsius
+    return print(f"{Fahrenheit} degrees fahrenheit = {Celsius} degrees Celsius")
 
-Celsius = 100
+celsius_to_fahrenheit(100)
 
-Fahrenheit = celsius_to_fahrenheit(Celsius)
+fahrenheit_to_celsius(200)
 
-print(f"{Celsius} degrees Celsius = {Fahrenheit} degrees Fahrenheit")
-
-
-Fahrenheit = 200
-
-Celsius = fahrenheit_to_celsius(Fahrenheit)
-
-print(f"{Fahrenheit} degrees fahrenheit = {Celsius} degrees Celsius")
