097539358c8d4f391a92946f669b28deaf580cbd
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-06-20 04:24:38 +0200

    Update and rename task_0.7.py to task_0_7.py

--- task_0_7.py
+++ task_0_7.py
@@ -6,7 +6,7 @@ def celsius_to_fahrenheit(celsius):
 
 
 def fahrenheit_to_celsius(fahrenheit):
-    celsius = fahrenheit - 32 * (5/9)
+    celsius = (fahrenheit - 32) * 5/9
     return print(f"{fahrenheit} degrees Fahrenheit = {celsius} degrees Celsius")
 
 celsius_to_fahrenheit(100)
