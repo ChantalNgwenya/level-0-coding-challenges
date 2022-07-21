c19af243b3734374a6af207912d0141f623de275
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-07-21 13:36:47 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,10 +1,12 @@
 def area_of_triangle(num1,num2,num3):
-    if( num1 < 0  or num2 < 0 or num3 < 0 or num1+num3 <=num2 or num2+num3 <=num1 ):
-        print ("Not a triangle")
-        return None
     semi_perimeter = (num1 + num2 + num3) /2  
     
     area = (semi_perimeter* ( semi_perimeter-num1)* (semi_perimeter - num2)* (semi_perimeter -num3))** 0.5
-    print("Area of a triangle is %f" %area)
+    
+    if( num1 < 0  or num2 < 0 or num3 < 0 or num1+num3 <=num2 or num2+num3 <=num1 ):
+        print ("Not a triangle, its area is %f" %area)
+        
+    else:
+        print("Area of a triangle is %f" %area)
 
 area_of_triangle(5,6,7)
