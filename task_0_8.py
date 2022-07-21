ba6be96a5be7f3e5309de534f3e95ed476497813
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-07-21 14:14:42 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,18 +1,18 @@
-
 def convert(sec):
-    
-
     hour = sec // 60
     sec %= 60 
     minutes = sec 
-    if (hour==1 and minutes==1):
+    if(hour==1 and minutes==1):
         print(f"{hour} hour,{minutes} minute")
     elif(hour==1 and minutes>1):
         print(f"{hour} hour, {minutes} minutes")
-    elif(hour >1 and minutes ==1):
+    elif(hour >2 and minutes ==1):
         print(f"{hour} hours, {minutes} minute")
-    else:
+    elif(hour<2 and minutes==1):
+        print(f"{hour} hour,{minutes} minute")
+    else:    
         print(f"{hour} hours, {minutes} minutes")
+         
     
 sec = 71
 convert(sec)
