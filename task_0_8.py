ebc73bab8956e39c73cd6d2e18861e24ccd14b21
Chantal Nosipho Ngwenya <102623467+ChantalNgwenya@users.noreply.github.com>
2022-07-28 23:26:39 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -3,18 +3,19 @@ def convert(number):
     number %= 60 
     minutes = number 
     if(hour==0 and minutes==1):
-        return f"{hour} hour,{minutes} minute"
+        return f"'{hour} hour','{minutes} minute'"
     elif(hour==1 and minutes==0):
-        return f'{hour} hour, {minutes} minutes'
+        return f"'{hour} hour', '{minutes} minutes'"
     elif (hour==0 and minutes>1):
         return f'{hour} hour, {minutes} minutes'
     elif(hour==1 and minutes>1):
-        return f"{hour} hour, {minutes} minutes"
+        return f"'{hour} hour', '{minutes} minutes'"
     elif(hour>1 and minutes ==1):
-        return f"{hour} hours, {minutes} minute"
+        return f"'{hour} hours', '{minutes} minute'"
     elif (hour==1 and hour==1):
-        return f'{hour} hour, {minutes} minute'
+        return f"'{hour} hour', '{minutes} minute'"
     else:    
-        return f"{hour} hours, {minutes} minutes"
+        return f"'{hour} hours', '{minutes} minutes'"
          
+      
 print(convert(71))
